13830824730dc3eef4d3be7b9ff339a64fc5903a
Nui Narongwet <narongwet.m@gmail.com>
2020-04-06 00:04:31 +0700

    use release hal

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,30 @@
 #![no_main]
 #![no_std]
 
-mod aux;
-
-use aux::{entry, Delay};
 use stm32f4xx_hal::prelude::*;
+
+use aux::entry;
+
+mod aux;
 
 #[entry]
 fn main() -> ! {
-    let (mut delay, mut gpiog) = aux::init();
+    let (mut delay, gpiog) = aux::init();
 
-    let duration = 1 * 200 as u32;
+    let duration = 1 * 250 as u32;
     gpiog.moder.modify(|_, w| w.moder13().output());
     gpiog.moder.modify(|_, w| w.moder14().output());
-    let mut i = 0u32;
-    let mut off_counter = (0..3).cycle();
+    let off_counter = (0..3).cycle();
     for i in off_counter {
         delay.delay_ms(duration);
         match i {
             0 => {
                 gpiog.odr.modify(|_, w| w.odr13().clear_bit());
                 gpiog.odr.modify(|_, w| w.odr14().clear_bit());
-            },
+            }
             1 => {
                 gpiog.odr.modify(|_, w| w.odr13().set_bit());
-            },
+            }
             _ => {
                 gpiog.odr.modify(|_, w| w.odr14().set_bit());
             }

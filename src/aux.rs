a22684f47d2b5f3d6d2ba85f2e516f56f2ad6301
Nui Narongwet <narongwet.m@gmail.com>
2020-04-07 01:10:19 +0700

    Try to print value from Rng

--- src/aux.rs
+++ src/aux.rs
@@ -1,24 +1,30 @@
+extern crate panic_halt;
+
+// panic handler
+pub use cortex_m_rt::entry;
+use stm32f4::stm32f429::{self, GPIOG, gpiok};
 pub use stm32f4xx_hal::delay::Delay;
 use stm32f4xx_hal::prelude::*;
-use stm32f4xx_hal::stm32;
-use stm32f4::stm32f429;
+use stm32f4xx_hal::rng::Rng;
 
-extern crate panic_halt; // panic handler
-pub use cortex_m_rt::entry;
-use stm32f4::stm32f429::{GPIOG, gpiok};
-
-pub fn init() -> (Delay, GPIOG) {
+pub fn init() -> (Delay, GPIOG, Rng) {
     let cp = cortex_m::Peripherals::take().unwrap();
     let dp = stm32f429::Peripherals::take().unwrap();
 
     let gpiog = dp.GPIOG;
 
-    dp.RCC.ahb1enr.write(|w|w.gpiogen().set_bit());
+    dp.RCC.ahb1enr.write(|w| w.gpiogen().set_bit());
+    dp.RCC.ahb2enr.write(|w| w.rngen().set_bit());
 
     let mut rcc = dp.RCC.constrain();
-    let clocks = rcc.cfgr.freeze();
+
+    let clocks = rcc.cfgr
+        .require_pll48clk() // this is for rng
+        .freeze();
+
+    let rng = dp.RNG.constrain(clocks.clone());
 
     let delay = Delay::new(cp.SYST, clocks);
 
-    (delay, gpiog)
+    (delay, gpiog, rng)
 }
--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,25 @@
 use stm32f4xx_hal::prelude::*;
 
 use aux::entry;
+use rand_core::RngCore;
+use cortex_m_semihosting::hprintln;
 
 mod aux;
 
 #[entry]
 fn main() -> ! {
-    let (mut delay, gpiog) = aux::init();
+    let (mut delay, gpiog, mut rng) = aux::init();
 
     let duration = 1 * 250 as u32;
     gpiog.moder.modify(|_, w| w.moder13().output());
     gpiog.moder.modify(|_, w| w.moder14().output());
     let off_counter = (0..3).cycle();
+
+    for _ in 0..10 {
+        let v = rng.next_u32();
+        hprintln!("Random num: {}", v);
+    }
+
     for i in off_counter {
         delay.delay_ms(duration);
         match i {
